4a38288f0a90f831dc92a8682e7dde29e5348aa2
Marc Mamin <m.mamin@intershop.de>
2022-10-21 08:53:08 +0200

    add pg function testcases.tc_omt_drop_sso_use(p_email)

--- src/main/resources/db/migration/R__tc_omt_dropusr.sql
+++ src/main/resources/db/migration/R__tc_omt_dropusr.sql
@@ -1,76 +1,106 @@
+	
 CREATE OR REPLACE FUNCTION testcases.tc_omt_dropusr(p_usrname character varying)
  RETURNS character varying
  LANGUAGE plpgsql
 AS $$
        
 DECLARE
-   usr_id int;
    users record;
-       
 BEGIN
 
 --to reduce the risk of FK exceptions during parallel tests.
 PERFORM pg_advisory_xact_lock(5,5);
 
-
  FOR users IN SELECT id FROM oms."UserDO" WHERE "accountName" = p_usrname
  LOOP
-
-
-
-
-   DELETE FROM "CSVReturnImportRowDO"              WHERE  "csvImportRef"          IN (SELECT id FROM "CSVImportDO"          WHERE "userRef"=users.id);
-   DELETE FROM "ApprovalResponseStateHistoryDO"    WHERE  "approvalResponseRef"   IN (SELECT id FROM "ApprovalResponseDO"   WHERE "userRef"=users.id);
-   DELETE FROM "CSVDispatchImportRowDO"            WHERE  "csvImportRef"          IN (SELECT id FROM "CSVImportDO"          WHERE "userRef"=users.id);
-
-   DELETE FROM oms."ApprovalResponseDO"                            WHERE "userRef" =users.id;
-   DELETE FROM oms."ApprovalResponseStateHistoryDO"                WHERE "userRef" =users.id;
-   DELETE FROM oms."CSVImportDO"                                   WHERE "userRef" =users.id;
-   DELETE FROM oms."DispatchStateHistoryDO"                        WHERE "userRef" =users.id;
-   DELETE FROM oms."DispatchTransmissionStateHistoryDO"            WHERE "userRef" =users.id;
-   DELETE FROM oms."DocumentStateHistoryDO"                        WHERE "userRef" =users.id;
-   DELETE FROM oms."DocumentTransmissionStateHistoryDO"            WHERE "userRef" =users.id;
-   DELETE FROM oms."EventControlStateHistoryDO"                    WHERE "userRef" =users.id;
-   DELETE FROM oms."InvoicingStateHistoryDO"                       WHERE "userRef" =users.id;
-   DELETE FROM oms."InvoicingTransmissionStateHistoryDO"           WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderCancelStateHistoryDO"                     WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderNoteDO"                                   WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderPosStateHistoryDO"                        WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderStateHistoryDO"                           WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderSupplierEvaluationStateHistoryDO"         WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderTransmissionStateHistoryDO"               WHERE "userRef" =users.id;
-   DELETE FROM oms."PaymentNotificationStateHistoryDO"             WHERE "userRef" =users.id;
-   DELETE FROM oms."PaymentNotificationTransmissionStateHistoryDO" WHERE "userRef" =users.id;
-   DELETE FROM oms."ProcessControlStateHistoryDO"                  WHERE "userRef" =users.id;
-   DELETE FROM oms."ResponseStateHistoryDO"                        WHERE "userRef" =users.id;
-   DELETE FROM oms."ResponseTransmissionStateHistoryDO"            WHERE "userRef" =users.id;
-   DELETE FROM oms."ReturnAnnouncementTransmissionStateHistoryDO"  WHERE "userRef" =users.id;
-   DELETE FROM oms."ReturnRequestStateHistoryDO"                   WHERE "userRef" =users.id;
-   DELETE FROM oms."ReturnRequestTransmissionStateHistoryDO"       WHERE "userRef" =users.id;
-   DELETE FROM oms."ReturnStateHistoryDO"                          WHERE "userRef" =users.id;
-   DELETE FROM oms."ReturnTransmissionStateHistoryDO"              WHERE "userRef" =users.id;
-   DELETE FROM oms."ShopCustomerMailTransmissionStateHistoryDO"    WHERE "userRef" =users.id;
-   DELETE FROM oms."User2OrganizationDO"                           WHERE "userRef" =users.id;
-   DELETE FROM oms."User2Role2OrganizationDO"                      WHERE "userRef" =users.id;
-   DELETE FROM oms."User2RoleDO"                                   WHERE "userRef" =users.id;
-   DELETE FROM oms."UserLoginHistoryDO"                            WHERE "userRef" =users.id;
-   DELETE FROM oms."UserPreferencesDO"                             WHERE "userRef" =users.id;
-   DELETE FROM oms."UserSessionDO"                                 WHERE "userRef" =users.id;
- 
-   DELETE FROM oms."ReturnAnnouncementStateHistoryDO"              WHERE "userRef" =users.id;
-  
-   DELETE FROM omt."Page2Widget4UserDTO"                           WHERE "userRef" =users.id;
-   DELETE FROM omt."Table2Column4UserDTO"                          WHERE "userRef" =users.id;
-
-   DELETE FROM oms."OrderChangeRequestStateHistoryDO"              WHERE "userRef" =users.id;
-   DELETE FROM oms."OrderChangeRequestDO"                          WHERE "userRef" =users.id;
-   DELETE FROM customer."CustomerDeletionHistoryDO"                WHERE "userRef" =users.id;
-   DELETE FROM oms."UserDO"                                        WHERE id = users.id;
-
+   PERFORM testcases.tc_omt_dropusr(users.id);
  END LOOP;
-
  Return 'ok';
+END;
+$$;
+comment on function testcases.tc_omt_dropusr(varchar) is'delete  all data bound to the local user(s) with the given accountName';
 
+
+CREATE OR REPLACE FUNCTION testcases.tc_omt_drop_sso_use(p_email character varying)
+ RETURNS character varying
+ LANGUAGE plpgsql
+AS $$
+       
+DECLARE
+   users record;
+BEGIN
+
+--to reduce the risk of FK exceptions during parallel tests.
+PERFORM pg_advisory_xact_lock(5,5);
+
+ FOR users IN SELECT id FROM oms."UserDO" WHERE "externalId" is not null and email=p_email
+ LOOP
+   PERFORM testcases.tc_omt_dropusr(users.id);
+ END LOOP;
+ Return 'ok';
 END;
 $$;
 
+comment on function testcases.tc_omt_drop_sso_use is'delete  all data bound to the sso user(s) with the given email';
+
+
+
+CREATE OR REPLACE FUNCTION testcases.tc_omt_dropusr(p_userid int8)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $$
+              
+BEGIN
+
+
+   DELETE FROM "CSVReturnImportRowDO"              WHERE  "csvImportRef"          IN (SELECT id FROM "CSVImportDO"          WHERE "userRef"=p_userid);
+   DELETE FROM "ApprovalResponseStateHistoryDO"    WHERE  "approvalResponseRef"   IN (SELECT id FROM "ApprovalResponseDO"   WHERE "userRef"=p_userid);
+   DELETE FROM "CSVDispatchImportRowDO"            WHERE  "csvImportRef"          IN (SELECT id FROM "CSVImportDO"          WHERE "userRef"=p_userid);
+
+   DELETE FROM oms."ApprovalResponseDO"                            WHERE "userRef" =p_userid;
+   DELETE FROM oms."ApprovalResponseStateHistoryDO"                WHERE "userRef" =p_userid;
+   DELETE FROM oms."CSVImportDO"                                   WHERE "userRef" =p_userid;
+   DELETE FROM oms."DispatchStateHistoryDO"                        WHERE "userRef" =p_userid;
+   DELETE FROM oms."DispatchTransmissionStateHistoryDO"            WHERE "userRef" =p_userid;
+   DELETE FROM oms."DocumentStateHistoryDO"                        WHERE "userRef" =p_userid;
+   DELETE FROM oms."DocumentTransmissionStateHistoryDO"            WHERE "userRef" =p_userid;
+   DELETE FROM oms."EventControlStateHistoryDO"                    WHERE "userRef" =p_userid;
+   DELETE FROM oms."InvoicingStateHistoryDO"                       WHERE "userRef" =p_userid;
+   DELETE FROM oms."InvoicingTransmissionStateHistoryDO"           WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderCancelStateHistoryDO"                     WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderNoteDO"                                   WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderPosStateHistoryDO"                        WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderStateHistoryDO"                           WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderSupplierEvaluationStateHistoryDO"         WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderTransmissionStateHistoryDO"               WHERE "userRef" =p_userid;
+   DELETE FROM oms."PaymentNotificationStateHistoryDO"             WHERE "userRef" =p_userid;
+   DELETE FROM oms."PaymentNotificationTransmissionStateHistoryDO" WHERE "userRef" =p_userid;
+   DELETE FROM oms."ProcessControlStateHistoryDO"                  WHERE "userRef" =p_userid;
+   DELETE FROM oms."ResponseStateHistoryDO"                        WHERE "userRef" =p_userid;
+   DELETE FROM oms."ResponseTransmissionStateHistoryDO"            WHERE "userRef" =p_userid;
+   DELETE FROM oms."ReturnAnnouncementTransmissionStateHistoryDO"  WHERE "userRef" =p_userid;
+   DELETE FROM oms."ReturnRequestStateHistoryDO"                   WHERE "userRef" =p_userid;
+   DELETE FROM oms."ReturnRequestTransmissionStateHistoryDO"       WHERE "userRef" =p_userid;
+   DELETE FROM oms."ReturnStateHistoryDO"                          WHERE "userRef" =p_userid;
+   DELETE FROM oms."ReturnTransmissionStateHistoryDO"              WHERE "userRef" =p_userid;
+   DELETE FROM oms."ShopCustomerMailTransmissionStateHistoryDO"    WHERE "userRef" =p_userid;
+   DELETE FROM oms."User2OrganizationDO"                           WHERE "userRef" =p_userid;
+   DELETE FROM oms."User2Role2OrganizationDO"                      WHERE "userRef" =p_userid;
+   DELETE FROM oms."User2RoleDO"                                   WHERE "userRef" =p_userid;
+   DELETE FROM oms."UserLoginHistoryDO"                            WHERE "userRef" =p_userid;
+   DELETE FROM oms."UserPreferencesDO"                             WHERE "userRef" =p_userid;
+   DELETE FROM oms."UserSessionDO"                                 WHERE "userRef" =p_userid;
+ 
+   DELETE FROM oms."ReturnAnnouncementStateHistoryDO"              WHERE "userRef" =p_userid;
+  
+   DELETE FROM omt."Page2Widget4UserDTO"                           WHERE "userRef" =p_userid;
+   DELETE FROM omt."Table2Column4UserDTO"                          WHERE "userRef" =p_userid;
+
+   DELETE FROM oms."OrderChangeRequestStateHistoryDO"              WHERE "userRef" =p_userid;
+   DELETE FROM oms."OrderChangeRequestDO"                          WHERE "userRef" =p_userid;
+   DELETE FROM customer."CustomerDeletionHistoryDO"                WHERE "userRef" =p_userid;
+   DELETE FROM oms."UserDO"                                        WHERE id = p_userid;
+
+
+END;
+$$;
