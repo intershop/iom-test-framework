66f25b9e3fd94e31609666491a0aa9c278f021b9
Marc Mamin <m.mamin@intershop.de>
2023-01-16 15:05:32 +0100

    test: add serviceHandlers and Mappers (#82368)

---
+++ src/main/java/com/intershop/oms/test/servicehandler/orderservice/v2_3/mapping/AddressLocationPOBoxMapper.java
@@ -0,0 +1,19 @@
+package com.intershop.oms.test.servicehandler.orderservice.v2_3.mapping;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.intershop.oms.rest.order.v2_3.model.AddressLocationPOBox;
+import com.intershop.oms.test.businessobject.address.OMSAddressLocationPOBox;
+
+@Mapper
+public interface AddressLocationPOBoxMapper
+{
+    AddressLocationPOBoxMapper INSTANCE = Mappers.getMapper(AddressLocationPOBoxMapper.class);
+
+    OMSAddressLocationPOBox fromApiAddressLocationPOBox(AddressLocationPOBox addressLocationPOBox);
+
+    @InheritInverseConfiguration
+    public abstract AddressLocationPOBox toApiAddressLocationPOBox(OMSAddressLocationPOBox omsAddressLocationPOBox);
+}
