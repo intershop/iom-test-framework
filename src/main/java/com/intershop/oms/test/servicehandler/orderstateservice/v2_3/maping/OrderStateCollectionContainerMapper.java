0d98e34ae582c33e9b3c57a9274e58a5155c9e04
Marc Mamin <m.mamin@intershop.de>
2023-01-24 08:49:50 +0100

    chore: do not use duplicated mappers (#47)

--- src/main/java/com/intershop/oms/test/servicehandler/orderstateservice/v2_3/maping/OrderStateCollectionContainerMapper.java
+++ src/main/java/com/intershop/oms/test/servicehandler/orderstateservice/v2_3/maping/OrderStateCollectionContainerMapper.java
@@ -1,11 +1,15 @@
 package com.intershop.oms.test.servicehandler.orderstateservice.v2_3.maping;
 
 import com.intershop.oms.rest.order.v2_3.model.OrderStateCollectionContainer;
+import com.intershop.oms.rest.order.v2_3.model.OrderStateCollectionContainer;
 import com.intershop.oms.test.businessobject.orderstate.OMSOrderStateCollectionContainer;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = { OrderStatePositionMapper.class, TaxMapper.class, AddressInvoiceLocationMapper.class, AddressShippingLocationMapper.class })
+@Mapper(uses = { OrderStatePositionMapper.class, 
+                TaxMapper.class, 
+                com.intershop.oms.test.servicehandler.orderservice.v2_3.mapping.AddressInvoiceLocationMapper.class, 
+                com.intershop.oms.test.servicehandler.orderservice.v2_3.mapping.AddressShippingLocationMapper.class })
 public interface OrderStateCollectionContainerMapper
 {
     OrderStateCollectionContainerMapper INSTANCE = Mappers.getMapper(OrderStateCollectionContainerMapper.class);
