5ee94685e02ab771130de30528c7ddac76729627
Götz Tänzer <G.Taenzer@intershop.de>
2023-06-05 12:14:13 +0200

    feat: support order API 2.4 (#66)

---
+++ src/main/java/com/intershop/oms/test/servicehandler/orderservice/v2_4/mapping/OrderPositionReturnedMixIn.java
@@ -0,0 +1,17 @@
+package com.intershop.oms.test.servicehandler.orderservice.v2_4.mapping;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.intershop.oms.rest.order.v2_4.model.OrderPositionReturned;
+import com.intershop.oms.test.servicehandler.orderservice.v2_4.mapping.OrderPositionReturnedDeserializer;
+
+/**
+ * registering a module at the mapper did not override the local deserializer
+ * annotation in OrderPositionReturned so this helper class can be mixed in to
+ * force the usage of custom deserializers since the generated ones can't handle
+ * the missing type discriminator...
+ */
+@JsonDeserialize(using = OrderPositionReturnedDeserializer.class)
+public abstract class OrderPositionReturnedMixIn 
+{
+
+}
